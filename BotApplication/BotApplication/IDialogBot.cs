7281ddddb14d2a5bb17fd01cf7f7ccf6d8174bc3
maleacristina <maleacristinaecaterina@gmail.com>
2018-07-03 12:20:54 +0300

    The first course

---
+++ BotApplication/BotApplication/IDialogBot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotApplication
+{
+    public interface IDialogBot
+    {
+        void ReceiveMessage(string message);
+    }
+}
